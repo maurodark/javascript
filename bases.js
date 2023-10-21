3867d73573669070340ce9fa766eb10e1ec215f8
Mauricio Peña <emauriciope@gmail.com>
2023-10-20 23:40:32 -0500

    Clases

--- bases.js
+++ bases.js
@@ -1,5 +1,5 @@
 // Listas 
-let myList = ["Mauro","Dani","Angel", 48]
+let myList = ["Mauro","Dani","Angel", 48, "Negro"]
 console.log(myList)
 console.log(myList[2])
 
@@ -12,5 +12,34 @@ console.log(mySet)
 let myMaps = new Map ([["Mauricio", 48], ["Javier" , 48], ["Angela", 43]])
 myMaps.set("Laura", 33)
 console.log(myMaps)
+    //Trae el valor de la clave con get
+console.log(myMaps.get("Mauricio"))
 
 
+// Bucles
+
+for (const value of myList){
+    //console.log(myList)
+    console.log(value)
+}
+
+let myCounter = 0 
+
+while (myCounter < myList.length ){
+    console.log("myCounter : " + myList[myCounter])
+    myCounter++
+}
+
+
+//Clases 
+// van en UpperCase
+class MyClass {
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+}
+let myClass = new MyClass("Male", 15)
+console.log(myClass)
+console.log("-------")
+console.log(myClass.age)
