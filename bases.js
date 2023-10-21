27068f2da2aeda0af6d4f6bdb7ab5d928f2207b0
Mauricio Peña <emauriciope@gmail.com>
2023-10-20 23:18:23 -0500

    Nuevos ejemplos

--- bases.js
+++ bases.js
@@ -1,4 +1,9 @@
+// Set
+let mySet = new Set (["Mauro","Angel","Victory",48])
+mySet.add("Vane")
+console.log(mySet)
+
 // Maps
-let myMaps = newMap ([["Mauricio, 48"], ["Javier , 48"], ["Angela", 43]])
+let myMaps = new Map ([["Mauricio", 48], ["Javier" , 48], ["Angela", 43]])
 myMaps.set("Laura", 33)
 console.log(myMaps)
